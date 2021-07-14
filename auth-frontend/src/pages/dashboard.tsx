e561a0f97c54582c33221beda037c6c8467e8dc3
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2021-07-14 11:53:00 -0300

    Realizando refresh do token

--- auth-frontend/src/pages/dashboard.tsx
+++ auth-frontend/src/pages/dashboard.tsx
@@ -1,7 +1,13 @@
+import { useEffect } from "react"
 import { useAuth } from "../contexts/AuthContext"
+import { api } from "../services/api"
 
 export default function dashboard() {
   const { user } = useAuth()
+
+  useEffect(() => {
+    api.get('me').then(response => console.log(response))
+  }, [])
 
   return (
     <h1>Dashboard: {user?.email}</h1>
