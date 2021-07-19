8dd0861dd624fe306b91f8f70d3c32c392ab74d6
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2021-07-19 18:35:00 -0300

    Redirecionamento pelo servidor

--- frontend/src/utils/withSSRAuth.ts
+++ frontend/src/utils/withSSRAuth.ts
@@ -1,19 +1,31 @@
 import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult } from "next";
-import { parseCookies } from "nookies";
+import { destroyCookie, parseCookies } from "nookies";
 
 export function withSSRAuth<P>(fn: GetServerSideProps<P>) {
   return async (ctx: GetServerSidePropsContext): Promise<GetServerSidePropsResult<P>> => {
-    const cookies = parseCookies()
+    const cookies = parseCookies(ctx)
 
     if(!cookies['nextauth.token']) {
       return {
         redirect: {
-          destination: '/dashboard',
+          destination: '/',
           permanent: false,
         }
       }
     }
 
-    return await fn(ctx)
+    try {
+      return await fn(ctx)
+    } catch (err) {
+      destroyCookie(ctx, 'nextauth.token')
+      destroyCookie(ctx, 'nextauth.refreshToken')
+  
+      return {
+        redirect: {
+          destination: '/',
+          permanent: false,
+        }
+      }
+    }
   }
 }
